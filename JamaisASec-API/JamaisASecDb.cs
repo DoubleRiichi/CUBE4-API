0cc9013fd926992ec6122ead3ecb5261e8976f0e
jim <doubleriichi@git.fr>
2024-12-16 11:29:07 +0100

    Marre des conflits

--- JamaisASec-API/JamaisASecDb.cs
+++ JamaisASec-API/JamaisASecDb.cs
@@ -13,12 +13,12 @@ public class Articles
 {
     public int ID { get; set; }
     public string Nom { get; set; }
-    public int Quantité { get; set; }
+    public int Quantite { get; set; }
     public string Image { get; set; }
     public int Prix_unitaire { get; set; }
     public int Colisage { get; set; }
-    public int Quantité_Min { get; set; }
-    public int Année { get; set; }
+    public int Quantite_Min { get; set; }
+    public int Annee { get; set; }
     public string Description { get; set; }
 
     public int Familles_ID { get; set; }
