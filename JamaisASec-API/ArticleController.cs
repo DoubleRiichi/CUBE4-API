ebd1c7216d9cfc98503a2b48163338b6dc3711ba
Eliott <eliottcheminal@gmail.com>
2024-12-16 11:17:00 +0100

    code cleaning with model/controller

--- JamaisASec-API/ArticleController.cs
+++ JamaisASec-API/ArticleController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
+using JamaisASec_API.DB;
 
 
 
--- JamaisASec-API/JamaisASecDb.cs
+++ JamaisASec-API/JamaisASecDb.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
+using JamaisASec_API.DB;
 namespace JamaisASec_API;
 
 
@@ -10,19 +10,3 @@ public class JamaisASecDbContext : DbContext
     public DbSet<Articles> Articles { get; set; }
 }
 
-public class Articles
-{
-    public int ID { get; set; }
-    public string Nom { get; set; }
-    public int Quantité { get; set; }
-    public string Image { get; set; }
-    public int Prix_unitaire { get; set; }
-    public int Colisage { get; set; }
-    public int Quantité_Min { get; set; }
-    public int Année { get; set; }
-    public string Description { get; set; }
-
-    public int Familles_ID { get; set; }
-    public int Maisons_ID { get; set; }
-
-}
