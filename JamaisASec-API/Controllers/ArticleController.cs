965b04233ba023d845bd3b715f11226871f0cf9f
DoubleRiichi <DoubleRiichi>
2024-12-18 16:46:46 +0100

    Added crud for articles

--- JamaisASec-API/Controllers/ArticleController.cs
+++ JamaisASec-API/Controllers/ArticleController.cs
@@ -23,6 +23,7 @@ using System.Text.Json;
 
 
         }
+
         [HttpGet]
         [Route("[controller]/get/all")]
         public ActionResult GetAll()
@@ -35,6 +36,58 @@ using System.Text.Json;
 
             return Ok(data);
         }
+
+        [HttpGet]
+        [Route("[controller]/get/{id}")]
+        public ActionResult GetById(int id)
+        {
+
+            var data = _context.Articles.Find(id);
+
+            if(data != null)
+            {
+                return Ok(data);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPut]
+        [Route("[controller]/update/{id}")]
+        public ActionResult Update([FromBody] Articles article, int id)
+        {
+
+            if (article == null || article.ID != id)
+            {
+                return BadRequest();
+            }
+
+            var existingArticle = _context.Articles.Find(id);
+            if (existingArticle == null)
+            {
+                return NotFound();
+            }
+
+            existingArticle.Nom = article.Nom;
+            existingArticle.Quantite = article.Quantite;
+            existingArticle.Quantite_Min = article.Quantite_Min;
+            existingArticle.Image = article.Image;
+            existingArticle.Prix_unitaire = article.Prix_unitaire;
+            existingArticle.Colisage = article.Colisage;
+            existingArticle.Quantite_Min = article.Quantite_Min;
+            existingArticle.Annee = article.Annee;
+            existingArticle.Description = article.Description;
+            existingArticle.Familles_ID = article.Familles_ID;
+            existingArticle.Maisons_ID = article.Maisons_ID;
+
+
+
+            _context.Articles.Update(existingArticle);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+
         [HttpPost]
         [Route("[controller]/create")]
         public IActionResult Create([FromBody] Articles article)
@@ -42,6 +95,23 @@ using System.Text.Json;
             _context.Articles.Add(article);
             _context.SaveChanges();
             return CreatedAtAction(nameof(GetAll), new { id = article.ID }, article);
+        }
+
+
+        [HttpDelete]
+        [Route("delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            var article = _context.Articles.Find(id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            _context.Articles.Remove(article);
+            _context.SaveChanges();
+
+            return NoContent();
         }
 
     }
