dc7ec1d0384ddc5578a3a66e8215fbf7a6414a80
hanan3889 <rebaia_hanan@yahoo.fr>
2024-12-18 11:51:10 +0100

    add CRUD method to MaisonsController

--- JamaisASec-API/Controllers/MaisonsController.cs
+++ JamaisASec-API/Controllers/MaisonsController.cs
@@ -1,49 +1,91 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using JamaisASec;
 using JamaisASec.Models;
 using System.Text.Json;
-
-
+using System.Linq;
 
 namespace JamaisASec.Controllers
-
-
-
 {
     [ApiController]
+    [Route("api/[controller]")]
     public class MaisonsController : ControllerBase
     {
         private readonly JamaisASecDbContext _context;
 
-
         public MaisonsController(JamaisASecDbContext context)
-
         {
             _context = context;
-
-
         }
+
         [HttpGet]
-        [Route("[controller]/get/all")]
+        [Route("get/all")]
         public ActionResult GetAll()
         {
-
-            var data = _context.Maisons;
-
-           
-            
-
+            var data = _context.Maisons.ToList();
             return Ok(data);
         }
 
-        [HttpPost]
-        [Route("[controller]/create")]
-        public IActionResult Create([FromBody] Maisons maison)
+        [HttpGet]
+        [Route("get/{id}")]
+        public ActionResult GetById(int id)
         {
-            _context.Maisons.Add(maison);
-            _context.SaveChanges();
-            return CreatedAtAction(nameof(GetAll), new { id = maison.ID }, maison);
+            var maison = _context.Maisons.Find(id);
+            if (maison == null)
+            {
+                return NotFound();
+            }
+            return Ok(maison);
         }
 
+        [HttpPost]
+        [Route("create")]
+        public IActionResult Create([FromBody] Maisons maison)
+        {
+            if (maison == null)
+            {
+                return BadRequest("Maison data is null.");
+            }
+
+            _context.Maisons.Add(maison);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new { id = maison.ID }, maison);
+        }
+
+        [HttpPut]
+        [Route("update/{id}")]
+        public IActionResult Update(int id, [FromBody] Maisons maison)
+        {
+            if (maison == null || maison.ID != id)
+            {
+                return BadRequest();
+            }
+
+            var existingMaison = _context.Maisons.Find(id);
+            if (existingMaison == null)
+            {
+                return NotFound();
+            }
+
+            existingMaison.Nom = maison.Nom;
+        
+
+            _context.Maisons.Update(existingMaison);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            var maison = _context.Maisons.Find(id);
+            if (maison == null)
+            {
+                return NotFound();
+            }
+
+            _context.Maisons.Remove(maison);
+            _context.SaveChanges();
+            return NoContent();
+        }
     }
 }
